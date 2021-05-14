47c8afbf9d7dd7158296b42ca5413155436b1ca9
CloudyWhale <83542885+CloudyWhale@users.noreply.github.com>
2021-05-14 16:08:54 -0400

    Dark Mode

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-function CAVEMAN () {
-    window,location.href="Rok/";
+var darkmode = false;
+function CAVEMAN() {
+  window, (location.href = "Rok/");
 }
 
-function ToggleDarkMode () {
-
+function ToggleDarkMode() {
+  if (darkmode == false) {
+    darkmode = true;
+  } else {
+    darkmode = false;
+  }
+  if (darkmode == true) {
+    document.getElementById("title").style.color = "white";
+    document.getElementById("mode").style.filter = "invert(1)";
+    document.getElementById("topnav").style.backgroundColor = "#1e1e1e";
+    document.getElementById("body").style.backgroundColor = "#1e1e1e";
+    document.getElementById("Rok").style.color = "#353535";
+  } else {
+    document.getElementById("title").style.color = "black";
+    document.getElementById("mode").style.filter = "invert(0)";
+    document.getElementById("topnav").style.backgroundColor = "white";
+    document.getElementById("body").style.backgroundColor = "white";
+    document.getElementById("Rok").style.color = "#dddddd";
+  }
 }
