ca12d3ffb88bc6682b5c8215b5fe5c5d05caf28c
CloudyWhale <83542885+CloudyWhale@users.noreply.github.com>
2021-06-01 15:52:23 -0400

    Trying to save preferences

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var darkmode = false;
+var darkmode = window.localStorage.getItem('darkmode');
 function CAVEMAN() {
   window, (location.href = "Rok/");
 }
@@ -10,16 +10,16 @@ function ToggleDarkMode() {
     darkmode = false;
   }
   if (darkmode == true) {
+    window.localStorage.setItem('darkmode', true);
     document.getElementById("title").style.color = "white";
     document.getElementById("mode").style.filter = "invert(1)";
     document.getElementById("topnav").style.backgroundColor = "#1e1e1e";
     document.getElementById("body").style.backgroundColor = "#1e1e1e";
-    document.getElementById("Rok").style.color = "#353535";
   } else {
+    window.localStorage.setItem('darkmode', false);
     document.getElementById("title").style.color = "black";
     document.getElementById("mode").style.filter = "invert(0)";
     document.getElementById("topnav").style.backgroundColor = "white";
     document.getElementById("body").style.backgroundColor = "white";
-    document.getElementById("Rok").style.color = "#dddddd";
   }
 }
