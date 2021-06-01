57f849abe098e380688af11dfcc65add585d1d6b
CloudyWhale <83542885+CloudyWhale@users.noreply.github.com>
2021-06-01 16:04:00 -0400

    will this help?

--- script.js
+++ script.js
@@ -5,18 +5,18 @@ function CAVEMAN() {
 
 function ToggleDarkMode() {
   if (darkmode == false) {
-    darkmode = true;
+    darkmode = 'true';
   } else {
-    darkmode = false;
+    darkmode = 'false';
   }
-  if (darkmode == true) {
-    window.localStorage.setItem('darkmode', true);
+  if (darkmode == 'true') {
+    window.localStorage.setItem('darkmode', 'true');
     document.getElementById("title").style.color = "white";
     document.getElementById("mode").style.filter = "invert(1)";
     document.getElementById("topnav").style.backgroundColor = "#1e1e1e";
     document.getElementById("body").style.backgroundColor = "#1e1e1e";
   } else {
-    window.localStorage.setItem('darkmode', false);
+    window.localStorage.setItem('darkmode', 'false');
     document.getElementById("title").style.color = "black";
     document.getElementById("mode").style.filter = "invert(0)";
     document.getElementById("topnav").style.backgroundColor = "white";
