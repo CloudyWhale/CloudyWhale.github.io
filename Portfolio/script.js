ccb64bdd73d743cc2c61bd3435c2f0767ed47387
CloudyWhale <83542885+CloudyWhale@users.noreply.github.com>
2021-08-23 20:48:05 -0400

    Portfolio fix

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -8,8 +8,18 @@ function applymode() {
   if (darkmode == "true") {
     console.log(localStorage);
     document.getElementById("body").style.backgroundColor = "#1e1e1e";
+    document.getElementById("topnav").style.backgroundColor = "#1e1e1e";
+    document.getElementById("mode").style.filter = "invert(1)";
+    document.getElementById("Rok").style.color = "white";
+    document.getElementById("home").style.filter = "invert(1)";
+    document.getElementById("nothing").style.color = "white";
   } else {
     console.log(localStorage);
     document.getElementById("body").style.backgroundColor = "white";
+    document.getElementById("topnav").style.backgroundColor = "white";
+    document.getElementById("mode").style.filter = "invert(0)";
+    document.getElementById("Rok").style.color = "black";
+    document.getElementById("home").style.filter = "invert(0)";
+    document.getElementById("nothing").style.color = "black";
   }
 }
